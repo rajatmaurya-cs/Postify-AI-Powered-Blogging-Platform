b8b4cd1fec4a5602d6dda8e8e4a086f25d68f4d4
Rajat Maurya <rajatmaurya176@gmail.com>
2026-02-14 03:17:03 +0530

    querclient wrap with app.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,18 +6,22 @@ import App from "./App.jsx";
 import { AuthProvider } from "./Context/Authcontext.jsx";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// ✅ 1) Create QueryClient instance
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Toaster />
     <GoogleOAuthProvider clientId="24947228077-fb2k4t5clt6kgmtl0sk31ogi3ea0h3sr.apps.googleusercontent.com">
-      <BrowserRouter>
-      <AuthProvider>
-       
-          <App />
-        
-      </AuthProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   </StrictMode>
 );
