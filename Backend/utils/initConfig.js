2149b9b270b844745267a90a5226955d72e68178
Rajat Maurya <rajatmaurya176@gmail.com>
2026-02-18 21:53:50 +0530

    init config fixed

--- Backend/utils/initConfig.js
+++ Backend/utils/initConfig.js
@@ -1,19 +1,45 @@
+// import Config from "../Models/Config.js";
+
+// const initConfig = async () => {
+
+//    await Config.findOneAndUpdate(
+//       {},
+//       {}, 
+//       {
+//          upsert: true,
+//          new: true
+//       }
+//    );
+
+//    console.log("✅ Config initialized");
+// };
+
+// export default initConfig;
+
+
+
+
 import Config from "../Models/Config.js";
 
 const initConfig = async () => {
+  const config = await Config.findOneAndUpdate(
+    {},
+    {
+      $setOnInsert: {
+        aiEnabled: true,
+        dailyAiLimit: 5,
+        dailyappLimit: 50,
+        aiModel: "openai/gpt-oss-120b",
+        aiPerMinuteLimit: 2,
+      },
+      $set: {
+        aiPerMinuteLimit: 2, 
+      },
+    },
+    { upsert: true, new: true }
+  );
 
-   await Config.findOneAndUpdate(
-      {},
-      {}, 
-      {
-         upsert: true,
-         new: true
-      }
-   );
-
-   console.log("✅ Config initialized");
+  console.log("✅ Config initialized:", config);
 };
 
 export default initConfig;
-
-
