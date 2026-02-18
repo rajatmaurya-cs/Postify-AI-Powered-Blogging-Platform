a85294fc762f3b66dfb11279c07058d2c53d9209
Rajat Maurya <rajatmaurya176@gmail.com>
2026-02-18 22:21:33 +0530

    config inti

--- Backend/utils/initConfig.js
+++ Backend/utils/initConfig.js
@@ -1,31 +1,19 @@
-// import Config from "../Models/Config.js";
-
-// const initConfig = async () => {
-
-//    await Config.findOneAndUpdate(
-//       {},
-//       {}, 
-//       {
-//          upsert: true,
-//          new: true
-//       }
-//    );
-
-//    console.log("✅ Config initialized");
-// };
-
-// export default initConfig;
-
-
-
-
 import Config from "../Models/Config.js";
 
-const initConfig = async () => {
+const initConfig = async (req, res, next) => {
+  try {
+    const existing = await Config.findOne();
 
-    await Config.create();
-   
-  console.log("✅ Config initialized:")
+    if (!existing) {
+      await Config.create({});
+      console.log("✅ Config created for the first time");
+    }
+
+    next();
+  } catch (err) {
+    console.error("❌ Failed to init config:", err);
+    next(err);
+  }
 };
 
 export default initConfig;
