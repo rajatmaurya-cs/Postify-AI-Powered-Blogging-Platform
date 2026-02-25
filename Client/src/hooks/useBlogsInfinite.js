eff3515f753fc5f696c36fd32de46f2e5e2848d8
Rajat Maurya <rajatmaurya176@gmail.com>
2026-02-26 00:10:36 +0530

    fix: handle special characters in category query params

--- Client/src/hooks/useBlogsInfinite.js
+++ Client/src/hooks/useBlogsInfinite.js
@@ -4,18 +4,28 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 
 import API from "../Api/api";
 
-export function useBlogsInfinite({category = "All", limit = 3, isAdmin = false,
+export function useBlogsInfinite({ category = "All", limit = 3, isAdmin = false,
 }) {
   const endpoint = isAdmin ? "blog/admin/blogs" : "/blog/allblog";
 
   return useInfiniteQuery({
     queryKey: ["blogs", category, limit, isAdmin],
     queryFn: async ({ pageParam = 1 }) => {
-      const res = await API.get(
-        `${endpoint}?page=${pageParam}&limit=${limit}&category=${category}`
-      );
+
+
+      const res = await API.get(endpoint, {
+        params: {
+          page: pageParam,
+          limit,
+          category,
+        },
+      });
+
+
       if (!res.data?.success) throw new Error(res.data?.message || "Failed");
+
       return res.data;
+
     },
     getNextPageParam: (lastPage) =>
       lastPage?.hasMore ? lastPage?.nextPage : undefined,
